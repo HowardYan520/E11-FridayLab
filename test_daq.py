702cf9705337dbac76f0c856ca5786806a6170b9
Ali Hanks <alihanks@gmail.com>
2021-09-15 16:19:52 -0700

    Added outside data

--- test_daq.py
+++ test_daq.py
@@ -12,13 +12,12 @@ sensor=adafruit_bme280.Adafruit_BME280_I2C(i2c)
 interval=10
 sleep_time=1
 
-
 print(sys.argv)
 if len(sys.argv)>1:
 	interval=int(sys.argv[1])
 	if len(sys.argv)>2:
 		sleep_time=int(sys_argv[2])
-	
+
 
 start_time=time.time()
 current_time=start_time
--- weather_aq.py
+++ weather_aq.py
@@ -14,27 +14,39 @@ i2c=board.I2C()
 sensor=adafruit_bme280.Adafruit_BME280_I2C(i2c)
 ## The weather import section has ended
 
+import sys
 
+sleep_time=1
+run_time=10
+delay=0
+
+print(sys.argv)
+if len(sys.argv)>1:
+	interval=int(sys.argv[1])
+	if len(sys.argv)>2:
+		sleep_time=int(sys.argv[2])
+		if len(sys.argv)>3:
+			delay=int(sys.argv[3])
 
 
 current_time=time.time()
-run_time=10
+run_time=interval
 stop_time=current_time+run_time
-sleep_time=1
-
 
 ## This is for air quality (do NOT change)
 port=serial.Serial("/dev/serial0",baudrate=9600,timeout=1.5)
 ## The air quality section has ended 
 
 
-with open('weather_and_air.csv','w',newline='') as csvfile:
+with open('weather_and_air.csv','a',newline='') as csvfile:
 	csvwriter=csv.writer(csvfile,delimiter=',')
 	
 	header=['Time','PM_1','PM_2_5','PM_10','Temperature','Humidity','Pressure']
 	csvwriter.writerow(header)
 	
-	while current_time < stop_time+run_time:
+	time.sleep(delay)
+	
+	while current_time < stop_time+delay:
 		text=port.read(32)
 		PM_1=int.from_bytes(text[4:6],byteorder="big")
 		PM_2_5=int.from_bytes(text[6:8],byteorder="big")
